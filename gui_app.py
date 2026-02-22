ab41552cc7c3ca4abd7bc9e24fe29bdfdee8a61c
Daniel Molina <danielmolina16ia@gmail.com>
2026-02-21 23:32:18 -0400

    Cambio a version 1.1.0

--- gui_app.py
+++ gui_app.py
@@ -77,11 +77,11 @@ class PrintRedirector:
 class SigaeApp:
     def __init__(self, root):
         self.root = root
-        self.root.title("Gestor de Bajas y Notificaciones SIGAE v1.0.3")
+        self.root.title("Gestor de Bajas y Notificaciones SIGAE v1.1.0")
         self.root.state('zoomed')
         
         # --- CONTROL DE VERSIONES ---
-        self.VERSION_ACTUAL = "1.0.3"
+        self.VERSION_ACTUAL = "1.1.0"
         self.URL_VERSION = "https://raw.githubusercontent.com/dbloodmoon/Gestor-de-Bajas-y-Notificaciones-SIGAE/refs/heads/main/version.txt"
         self.URL_DESCARGA = "https://github.com/dbloodmoon/Gestor-de-Bajas-y-Notificaciones-SIGAE/releases/latest"
 
